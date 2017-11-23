816d7f665573abfbdb6847d76974349e31ca9d62
Rémy Gouello <rigouello@gmail.com>
2017-11-23 18:01:13 +0100

    up

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const WebClient = require('@slack/client').WebClient;
-// const token = process.env.SLACK_API_TOKEN || '';
+const moment = require('moment');
+const token = process.env.SLACK_APP_TOKEN || '';
 const clientId = process.env.SLACK_CLIENT_ID || '';
 const clientSecret = process.env.SLACK_CLIENT_SECRET || '';
 const express = require('express');
@@ -10,33 +11,41 @@ const scope = 'files:read,files:write:user';
 const state = 'ghjgj657JHK97HI';
 const redirectUri = 'http://localhost:3000/getToken';
 
-const web = new WebClient();
+app.get('/', function (req, res) {
+    res.send();
+});
 
 app.get('/getToken', function (req, res) {
     if (req.query.code && req.query.state === state) {
-        web.oauth.access(clientId, clientSecret, req.query.code, redirectUri, (err, res) => {
-            console.log(res.access_token);
+        const api = new WebClient();
+
+        api.oauth.access(clientId, clientSecret, req.query.code, redirectUri, (err, res) => {
         });
     } else {
         res.redirect(`https://slack.com/oauth/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&state=${state}`);
     }
 });
 
-// const web = new WebClient(token);
-// web.files.list((err, res) => {
-//     res.files.map((file) => {
-//         web.files.delete(file.id, (err, res) => {
-//             console.log(res);
-//         });
-//     });
-//     // console.log('Response', res);
-// });
-// web.chat.postMessage('C1232456', 'Hello there', function(err, res) {
-//   if (err) {
-//     console.log('Error:', err);
-//   } else {
-//     console.log('Message sent: ', res);
-//   }
-// });
+app.post('/delete-files', (req, res) => {
+    res.send('cool');
+});
+
+app.get('/delete-files', function (req, res) {
+    res.send('ok');
+    // const web = new WebClient(token);
+
+    // web.files.list({
+    //     // ts_to: moment().subtract(1, 'months').format('X'),
+    //     // ts_to: moment().subtract(20, 'seconds').format('X'),
+    // }, (err, res) => {
+    //     res.files.map((file) => {
+    //         web.users.profile.get({ user: file.user }, (err, user) => {
+    //             web.files.delete(file.id, (err, res) => {
+    //                 console.log(`Fichier "${file.title}", déposé par ${user.profile.real_name}, a été supprimé.`);
+    //             });
+    //         });
+    //     });
+    // });
+});
 
 app.listen(3000);
